436f5c40df6396fa76c08c55f50de4edd7c7c20a
Matthew Thompson <matthew@thompsons.id.au>
2021-05-05 18:32:27 +1000

    Store price data in JSON format - Bit nicer and easier to read the script code - Definitely easier to debug it! - Also tidied up some variable names and unused imports

--- app.py
+++ app.py
@@ -1,120 +1,119 @@
 import requests
 import json
-import pickle
 import os
-from time import strftime, localtime, gmtime
-from datetime import datetime
+from time import strftime, localtime
 
-# Create pickle file if it doesn't already exist
-if not os.path.isfile('data/lastprices.pkl'):
-    blankPrice = ["0", "0", "0", "0", "0", "0"]
-    with open('data/lastprices.pkl', 'wb') as file:
-        pickle.dump(blankPrice, file)
+# Create price data JSON file if it doesn't already exist
+if not os.path.isfile('data/priceData.json'):
+    blankPrice = {"E10":0,"U91":0,"U95":0,"U98":0,"Diesel":0,"LPG":0}
+    with open('data/priceData.json', 'w') as file:
+        json.dump(blankPrice, file)
         file.close()
 
-# Read last prices from pickle file
-with open('data/lastprices.pkl', 'rb') as file:
-    e10LastPrice, u91LastPrice, u95LastPrice, u98LastPrice, dieselLastPrice, lpgLastPrice = pickle.load(file)
+# Read last prices from JSON file
+with open('data/priceData.json', 'r') as file:
+    priceDataFile = json.load(file)
     file.close()
 
 # Set the URL for the ProjectZeroThree API
-apiUrl = 'https://projectzerothree.info/api.php?format=json'
+apiURL = 'https://projectzerothree.info/api.php?format=json'
 
 # Get current price data from the API and parse the JSON
-apiResponse = requests.post(apiUrl, headers={ 'User-Agent': 'FuelHook v2.0' })
-priceData = json.loads(apiResponse.text)
+apiResponse = requests.post(apiURL, headers={ 'User-Agent': 'FuelHook v2.1' })
+priceDataAPI = json.loads(apiResponse.text)
 
 # Get the prices and locations into variables
-lastUpdated = int(priceData['updated'])
+lastUpdated = int(priceDataAPI['updated'])
+regionPref = int(os.environ.get('REGION'))
 
-e10Price = float(priceData['regions'][int(os.environ.get('REGION'))]['prices'][0]['price'])
-e10Location = str(priceData['regions'][int(os.environ.get('REGION'))]['prices'][0]['suburb'] + ", " + priceData['regions'][int(os.environ.get('REGION'))]['prices'][0]['state'])
+priceE10 = float(priceDataAPI['regions'][regionPref]['prices'][0]['price'])
+locE10 = str(priceDataAPI['regions'][regionPref]['prices'][0]['suburb'] + ", " + priceDataAPI['regions'][regionPref]['prices'][0]['state'])
 
-u91Price = float(priceData['regions'][int(os.environ.get('REGION'))]['prices'][1]['price'])
-u91Location = str(priceData['regions'][int(os.environ.get('REGION'))]['prices'][1]['suburb'] + ", " + priceData['regions'][int(os.environ.get('REGION'))]['prices'][1]['state'])
+priceU91 = float(priceDataAPI['regions'][regionPref]['prices'][1]['price'])
+locU91 = str(priceDataAPI['regions'][regionPref]['prices'][1]['suburb'] + ", " + priceDataAPI['regions'][regionPref]['prices'][1]['state'])
 
-u95Price = float(priceData['regions'][int(os.environ.get('REGION'))]['prices'][2]['price'])
-u95Location = str(priceData['regions'][int(os.environ.get('REGION'))]['prices'][2]['suburb'] + ", " + priceData['regions'][int(os.environ.get('REGION'))]['prices'][2]['state'])
+priceU95 = float(priceDataAPI['regions'][regionPref]['prices'][2]['price'])
+locU95 = str(priceDataAPI['regions'][regionPref]['prices'][2]['suburb'] + ", " + priceDataAPI['regions'][regionPref]['prices'][2]['state'])
 
-u98Price = float(priceData['regions'][int(os.environ.get('REGION'))]['prices'][3]['price'])
-u98Location = str(priceData['regions'][int(os.environ.get('REGION'))]['prices'][3]['suburb'] + ", " + priceData['regions'][int(os.environ.get('REGION'))]['prices'][3]['state'])
+priceU98 = float(priceDataAPI['regions'][regionPref]['prices'][3]['price'])
+locU98 = str(priceDataAPI['regions'][regionPref]['prices'][3]['suburb'] + ", " + priceDataAPI['regions'][regionPref]['prices'][3]['state'])
 
-dieselPrice = float(priceData['regions'][int(os.environ.get('REGION'))]['prices'][4]['price'])
-dieselLocation = str(priceData['regions'][int(os.environ.get('REGION'))]['prices'][4]['suburb'] + ", " + priceData['regions'][int(os.environ.get('REGION'))]['prices'][4]['state'])
+priceDiesel = float(priceDataAPI['regions'][regionPref]['prices'][4]['price'])
+locDiesel = str(priceDataAPI['regions'][regionPref]['prices'][4]['suburb'] + ", " + priceDataAPI['regions'][regionPref]['prices'][4]['state'])
 
-lpgPrice = float(priceData['regions'][int(os.environ.get('REGION'))]['prices'][5]['price'])
-lpgLocation = str(priceData['regions'][int(os.environ.get('REGION'))]['prices'][5]['suburb'] + ", " + priceData['regions'][int(os.environ.get('REGION'))]['prices'][5]['state'])
+priceLPG = float(priceDataAPI['regions'][regionPref]['prices'][5]['price'])
+locLPG = str(priceDataAPI['regions'][regionPref]['prices'][5]['suburb'] + ", " + priceDataAPI['regions'][regionPref]['prices'][5]['state'])
 
 # Print the last updated time
 print("Prices last updated at: ", strftime("%a %d %b %Y %H:%M:%S", localtime(lastUpdated)))
 
 # Print the current prices
-print("\nBest E10 price: ", e10Price, "\nLocated at: ", e10Location)
-print("\nBest U91 price: ", u91Price, "\nLocated at: ", u91Location)
-print("\nBest U95 price: ", u95Price, "\nLocated at: ", u95Location)
-print("\nBest U98 price: ", u98Price, "\nLocated at: ", u98Location)
-print("\nBest diesel price: ", dieselPrice, "\nLocated at: ", dieselLocation)
-print("\nBest LPG price: ", lpgPrice, "\nLocated at: ", lpgLocation)
+print("\nBest E10 price: ", priceE10, "\nLocated at: ", locE10)
+print("\nBest U91 price: ", priceU91, "\nLocated at: ", locU91)
+print("\nBest U95 price: ", priceU95, "\nLocated at: ", locU95)
+print("\nBest U98 price: ", priceU98, "\nLocated at: ", locU98)
+print("\nBest diesel price: ", priceDiesel, "\nLocated at: ", locDiesel)
+print("\nBest LPG price: ", priceLPG, "\nLocated at: ", locLPG)
 
-## Configure the Discord webhook URL and post data
-webhookUrl = str(os.environ.get('WEBHOOK_URL'))
+## Configure the webhook URL and post data
+webhookURL = str(os.environ.get('WEBHOOK_URL'))
 content = ""
 
 # Alert on E10 change
-if e10Price != float(e10LastPrice):
-    if e10Price > float(e10LastPrice):
+if priceE10 != float(priceDataFile['E10']):
+    if priceE10 > float(priceDataFile['E10']):
         content += ":arrow_up:\t\t"
     else:
         content += ":arrow_down:\t\t"
-    content += ":fuelpump: E10\t\t:dollar: " + str(e10Price) + "\t\t:map: " + str(e10Location) + "\n\n"
+    content += ":fuelpump: E10\t\t:dollar: " + str(priceE10) + "\t\t:map: " + str(locE10) + "\n\n"
 
 # Alert on U91 change
-if u91Price != float(u91LastPrice):
-    if u91Price > float(u91LastPrice):
+if priceU91 != float(priceDataFile['U91']):
+    if priceU91 > float(priceDataFile['U91']):
         content += ":arrow_up:\t\t"
     else:
         content += ":arrow_down:\t\t"
-    content += ":fuelpump: U91\t\t:dollar: " + str(u91Price) + "\t\t:map: " + str(u91Location) + "\n\n"
+    content += ":fuelpump: U91\t\t:dollar: " + str(priceU91) + "\t\t:map: " + str(locU91) + "\n\n"
 
 # Alert on U95 change
-if u95Price != float(u95LastPrice):
-    if u95Price > float(u95LastPrice):
+if priceU95 != float(priceDataFile['U95']):
+    if priceU95 > float(priceDataFile['U95']):
         content += ":arrow_up:\t\t"
     else:
         content += ":arrow_down:\t\t"
-    content += ":fuelpump: U95\t\t:dollar: " + str(u95Price) + "\t\t:map: " + str(u95Location) + "\n\n"
+    content += ":fuelpump: U95\t\t:dollar: " + str(priceU95) + "\t\t:map: " + str(locU95) + "\n\n"
 
 # Alert on U98 change
-if u98Price != float(u98LastPrice):
-    if u98Price > float(u98LastPrice):
+if priceU98 != float(priceDataFile['U98']):
+    if priceU98 > float(priceDataFile['U98']):
         content += ":arrow_up:\t\t"
     else:
         content += ":arrow_down:\t\t"
-    content += ":fuelpump: U98\t\t:dollar: " + str(u98Price) + "\t\t:map: " + str(u98Location) + "\n\n"
+    content += ":fuelpump: U98\t\t:dollar: " + str(priceU98) + "\t\t:map: " + str(locU98) + "\n\n"
 
 # Alert on Diesel change
-if dieselPrice != float(dieselLastPrice):
-    if dieselPrice > float(dieselLastPrice):
+if priceDiesel != float(priceDataFile['Diesel']):
+    if priceDiesel > float(priceDataFile['Diesel']):
         content += ":arrow_up:\t\t"
     else:
         content += ":arrow_down:\t\t"
-    content += ":fuelpump: Diesel\t\t:dollar: " + str(dieselPrice) + "\t\t:map: " + str(dieselLocation) + "\n\n"
+    content += ":fuelpump: Diesel\t\t:dollar: " + str(priceDiesel) + "\t\t:map: " + str(locDiesel) + "\n\n"
 
 # Alert on LPG change
-if lpgPrice != float(lpgLastPrice):
-    if lpgPrice > float(lpgLastPrice):
+if priceLPG != float(priceDataFile['LPG']):
+    if priceLPG > float(priceDataFile['LPG']):
         content += ":arrow_up:\t\t"
     else:
         content += ":arrow_down:\t\t"
-    content += ":fuelpump: LPG\t\t:dollar: " + str(lpgPrice) + "\t\t:map: " + str(lpgLocation) + "\n\n"
+    content += ":fuelpump: LPG\t\t:dollar: " + str(priceLPG) + "\t\t:map: " + str(locLPG) + "\n\n"
 
 if content != "":
     content += "Prices are correct as of " + strftime("%a %d %b %Y %H:%M:%S", localtime(lastUpdated)) + "\n@everyone"
-    requests.post(webhookUrl, data={ 'content': content })
+    requests.post(webhookURL, data={ 'content': content })
 
-# Write the current price to the pickle file
-with open('/opt/fuelhook/data/lastprices.pkl', 'wb') as file:
-    pickle.dump([e10Price, u91Price, u95Price, u98Price, dieselPrice, lpgPrice], file)
+# Write the current price to the JSON file
+with open('/opt/fuelhook/data/priceData.json', 'w') as file:
+    json.dump(priceDataFile, file)
     file.close()
 
 # Print script completion to log
